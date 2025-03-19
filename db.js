15f06a37a7b82dfb32a349db357e8ffaed5a47c0
T1ag031 <tiagoqueiros133@gmail.com>
2025-03-19 14:52:43 +0000

    19-03

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const { Client } = require('pg');
+/*const { Client } = require('pg');
 
 // Configuração do cliente PostgreSQL
 const client = new Client({
@@ -6,6 +6,20 @@ const client = new Client({
     host: 'localhost',
     database: 'personalpage',
     password: '123',
+});
+
+// Conecta ao banco de dados
+client.connect()
+    .then(() => console.log('Conectado ao banco de dados PostgreSQL com sucesso!'))
+    .catch(err => console.error('Erro ao conectar ao banco de dados:', err.stack));
+
+module.exports = client;*/
+
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL, 
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 // Conecta ao banco de dados
